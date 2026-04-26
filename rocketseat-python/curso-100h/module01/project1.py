26c87ad8a09bd1d6bd65340b76962272dbebb941
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2026-04-26 11:54:16 -0300

    v.26.04.26

--- rocketseat-python/curso-100h/module01/project1.py
+++ rocketseat-python/curso-100h/module01/project1.py
@@ -13,7 +13,7 @@ def add_task(tasks, name_task):
     task = {"task": name_task, "done": False}
     tasks.append(task)
 
-    print(f"Task {name_task} was successfully added")
+    print(f"Task '{name_task}' was successfully added")
 
     loading()
     return
@@ -26,7 +26,16 @@ def see_task(tasks):
         name_task = task['task']
         print(f"{i}. [{status}] {name_task}")
 
+def update_task(tasks, index_task, new_name_task):
+    index_adjusted = int(index_task) - 1
 
+    if index_adjusted >= 0 and index_adjusted < len(tasks):
+        tasks[index_adjusted]["task"] = new_name_task
+        print(f"Task {index_task} updated to {new_name_task}")
+        loading()
+    else: 
+        print("Index out of range. Try again.")
+    return
 
 tasks = []
 while True:
@@ -49,6 +58,13 @@ while True:
 
     elif esc == "2":
         see_task(tasks)
+
+    elif esc == "3":
+        see_task(tasks)
+        
+        index_task = input("\nType the number of the task you want to update\n[number] ")
+        new_name_task = input("-"*35 +"\nType the new name for this task\n[new name] ")
+        update_task(tasks, index_task, new_name_task)
 
     elif esc == "6":
         break
