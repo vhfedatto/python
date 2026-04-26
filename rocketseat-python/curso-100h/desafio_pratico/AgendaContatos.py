71a34c86f4fa9e09a9cc5599845d9df630769187
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2026-04-26 19:52:29 -0300

    project01 - final version

--- rocketseat-python/curso-100h/desafio_pratico/AgendaContatos.py
+++ rocketseat-python/curso-100h/desafio_pratico/AgendaContatos.py
@@ -12,7 +12,7 @@ def unknown_command(style):
 
     match style:
         case 1:
-            print("\n[?] Unknown command. Returning to Menu [?]")
+            print("\n[?] Unknown command. Returning to the Menu [?]")
         case 2:
             print("\n[?] Unknown command. Try again [?]")
         case _:
@@ -29,7 +29,7 @@ def title_screen():
     print(" | |___| (_) | | | | || (_| | (__| |_    |_|  ".center(64))
     print("  \\_____\\___/|_| |_|\\__\\__,_|\\___|\\__|        ".center(64))
 
-    print("\n>>> Welcome to Contact+ let's go to the Menu!")
+    print("\n>>> Welcome to Contact+! Let's go to the menu!")
 
 
 # Primeira função - Criar um contato.
@@ -46,10 +46,11 @@ def add_contact(contacts, ctt_name, ctt_mobile, ctt_email):
     system('cls')
     return
 
+
 # Segunda função - Ver lista de contatos.
 
 def see_contacts(contacts):
-    print("\n"+"List of Contacts".center(64))
+    print("\n"+">>> Contact List <<<".center(64))
     print("-"*64)
 
     for i, contact in enumerate(contacts, start=1):
@@ -60,85 +61,121 @@ def see_contacts(contacts):
 
         print(f"{i}. [{fav}] {ctt_name} ∗ {ctt_mobile} ∗ {ctt_email}\n"+"-"*64)
 
+def see_fav_contacts(contacts):
+    print("\n"+">>> Favorite Contacts <<<".center(64))
+    print("-"*64)
+
+    has_favorites = False
+
+    for i, contact in enumerate(contacts, start=1):
+        if contact["fav"]:
+            has_favorites = True
+            print(f"{i}. [⭐] {contact['name']} ∗ {contact['mobile']} ∗ {contact['email']}") # É para mostrar o index original mesmo. Isso é proposital. Posso mudar e criar uma lista apenas para os favoritos.
+            print("-" * 64)
+
+    if not has_favorites:
+        print("No favorite contacts found.")
+        print("-" * 64) 
+            
+
 # Terceira função - Editar contatos
 
 def edit_contact(contacts, ctt_index):
+    if not ctt_index.isdigit():
+        print("Invalid contact ID. Try again.")
+        return
+    
     index_adjusted = int(ctt_index) - 1
 
-    for i, contact in enumerate(contacts):
-        if i == index_adjusted:
-            ctt_name = contact['name'] 
-            ctt_mobile = contact['mobile']
-            ctt_email = contact['email']
-
-    if index_adjusted >= 0 and index_adjusted < len(contacts):
-        
-        print("\n========== EDITING A CONTACT ==========")
-        print(f">>> In '{ctt_name}'s' contact, what do you want to edit?\n")
-
-        print(f"1. [☻ ] {ctt_name}")
-        print(f"2. [☎ ] {ctt_mobile}")
-        print(f"3. [✉ ] {ctt_email}")
-        
-        if contacts[index_adjusted]["fav"] == True: 
-            print(f"4. [⭐] Favorited") 
-        else: 
-            print(f"4. [  ] Favorite")
-
-        opt = input("\n[option] ")
-
-        match opt:
-            case "1":
-                ctt_new_name = input("\n[new name] ")
-                contacts[index_adjusted]["name"] = ctt_new_name
-                print('\n'+'-'*64)
-                print(f"✓ Name successfully updated to '{ctt_new_name}' ✓".center(64))
-                print("-"*64)
-
-            case "2":
-                ctt_new_mobile = input("\n[new mobile] ").replace(")", "").replace("(", "").replace(" ", "")
-                contacts[index_adjusted]["mobile"] = ctt_new_mobile
-                print('\n'+'-'*64)
-                print(f"✓ Mobile successfully updated to '{ctt_new_mobile}' ✓".center(64))
-                print("-"*64)
-
-            case "3":
-                ctt_new_email = input("\n[new email] ").lower()
-                contacts[index_adjusted]["email"] = ctt_new_email
-                print('\n'+'-'*64)
-                print(f"✓ E-mail successfully updated to '{ctt_new_email}' ✓".center(64))
-                print("-"*64)
-
-            case "4":
-                if contacts[index_adjusted]["fav"] == False:
-                    contacts[index_adjusted]["fav"] = True
-                    print('\n'+'-'*64)
-                    print(f"✓ '{ctt_name}' was marked as 'FAVORITE' successfully ✓".center(64))
-                    print("-"*64)
-                else:
-                    contacts[index_adjusted]["fav"] = False
-                    print('\n'+'-'*64)
-                    print(f"✓ '{ctt_name}' was removed from 'FAVORITES' successfully ✓".center(64))
-                    print("-"*64)
-
-            case _:
-                unknown_command(1)
-     
-        loading('\n...')
-        system('cls')
-    else: 
+    if index_adjusted < 0 or index_adjusted >= len(contacts):
         print("Index out of range. Try again.")
+        return
+
+    contact = contacts[index_adjusted]
+    ctt_name = contact["name"]
+    ctt_mobile = contact["mobile"]
+    ctt_email = contact["email"]
+
+        
+    print("\n"+" EDITING A CONTACT ".center(64, '-')+"\n")
+    print(f">>> What would you like to edit in '{ctt_name}'s contact?\n")
+
+    print(f"1. [☻ ] {ctt_name}")
+    print(f"2. [☎ ] {ctt_mobile}")
+    print(f"3. [✉ ] {ctt_email}")
+    
+    if contacts[index_adjusted]["fav"] == True: 
+        print(f"4. [⭐] Favorited") 
+    else: 
+        print(f"4. [  ] Favorite")
+
+    opt = input("\n[option] ")
+
+    match opt:
+        case "1":
+            ctt_new_name = input("\n[new name] ")
+            contacts[index_adjusted]["name"] = ctt_new_name
+            print('\n'+'-'*64)
+            print(f"✓ Name successfully updated to '{ctt_new_name}' ✓".center(64))
+            print("-"*64)
+            loading('\n...')
+            system('cls')
+
+        case "2":
+            ctt_new_mobile = input("\n[new mobile] ").replace("-", "").replace(")", "").replace("(", "").replace(" ", "")
+            contacts[index_adjusted]["mobile"] = ctt_new_mobile
+            print('\n'+'-'*64)
+            print(f"✓ Mobile successfully updated to '{ctt_new_mobile}' ✓".center(64))
+            print("-"*64)
+            loading('\n...')
+            system('cls')
+
+        case "3":
+            ctt_new_email = input("\n[new email] ").lower()
+            contacts[index_adjusted]["email"] = ctt_new_email
+            print('\n'+'-'*64)
+            print(f"✓ E-mail successfully updated to '{ctt_new_email}' ✓".center(64))
+            print("-"*64)
+            loading('\n...')
+            system('cls')
+
+        case "4":
+            if contacts[index_adjusted]["fav"] == False:
+                contacts[index_adjusted]["fav"] = True
+                print('\n'+'-'*64)
+                print(f"✓ '{ctt_name}' was successfully marked as favorite ✓".center(64))
+                print("-"*64)
+            else:
+                contacts[index_adjusted]["fav"] = False
+                print('\n'+'-'*64)
+                print(f"✓ '{ctt_name}' was successfully removed from favorites ✓".center(64))
+                print("-"*64)
+            loading('\n...')
+            system('cls')
+        case _:
+            unknown_command(1)
     return
 
+
 # Quarta função - Deletar contatos
+
 def delete_ctt(contacts, ctt_index):
+    
+    if not ctt_index.isdigit():
+        print("Invalid contact ID. Try again.")
+        return
+    
     index_adjusted = int(ctt_index) - 1
+
+    if index_adjusted < 0 or index_adjusted >= len(contacts):
+        print("Index out of range. Try again.")
+        return
 
     ctt_name = contacts[index_adjusted]["name"]
     contacts.pop(index_adjusted)
 
     print('\n'+'-'*64)
-    print(f"✓ '{ctt_name}' was removed from your contacts list ✓".center(64))
+    print(f"✓ '{ctt_name}' was removed from your contact list ✓".center(64))
     print("-"*64)
     
     loading('\n...')
@@ -151,7 +188,7 @@ while True:
     title_screen()  
     print("\n"+"="*29+" MENU "+"="*29)
     print("[1] Add contact")
-    print("[2] See contacts list")       # Inclui ver lista de favoritos
+    print("[2] View contact list")       # Inclui ver lista de favoritos
     print("[3] Edit existing contact")   # Inclui marcar como favorito
     print("[4] Delete a contact") 
     print("[5] Exit") 
@@ -161,8 +198,7 @@ while True:
 
     match opt:
         case "1":
-            print("\n========== CREATING A CONTACT ==========")
-            print(">>> Let's add a Contact!\n")
+            print("\n"+" CREATING A CONTACT ".center(64, '-')+"\n")
             ctt_name = input("[☻] ")
             ctt_mobile = input("[☎ ] ").replace("-", "").replace(")", "").replace("(", "").replace(" ", "") # futuramente adicionar uma função para formatar todos os números de uma forma bonita.
 
@@ -171,8 +207,22 @@ while True:
             add_contact(contacts, ctt_name, ctt_mobile, ctt_email)
         
         case "2":
-            see_contacts(contacts)
-            
+            print("\n[1] See Favorites\n[2] See all contacts\n[3] << Back")
+            opt_3 = input("[option] ")
+
+            match opt_3:
+                case "1":
+                    see_fav_contacts(contacts)
+                case "2":
+                    see_contacts(contacts)
+                case "3":
+                    loading('\n...')
+                    system('cls')
+                    continue
+                case _:
+                    unknown_command(1)
+                    continue
+
             print("[1] ← Back")
             print("[2] Edit contact")
 
@@ -192,25 +242,24 @@ while True:
                     unknown_command(1)
 
         case "3":
-            see_contacts(contacts)
 
             if len(contacts) == 0:
-                print("[X] Add Contacts first [X]")
+                print("[X] Add contacts first [X]")
                 loading("\n...")
             
             else:
+                see_contacts(contacts)
                 print("\n>>> Which contact do you want to edit?")
                 ctt_index = input("[id] ")
                 edit_contact(contacts, ctt_index)
 
         case "4":
-            see_contacts(contacts)
-
             if len(contacts) == 0:
-                print("[X] Add Contacts first [X]")
+                print("[X] Add contacts first [X]")
                 loading("\n...")
             
             else:
+                see_contacts(contacts)
                 print("\n>>> Which contact do you want to delete?")
                 ctt_index = input("[id] ")
                 delete_ctt(contacts, ctt_index)
