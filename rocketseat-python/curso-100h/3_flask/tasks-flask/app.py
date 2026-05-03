7b0e5b3b1b240d223389b39363fbd0cc724d8cf9
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2026-05-03 12:05:10 -0300

    v.03.05.26

--- rocketseat-python/curso-100h/3_flask/tasks-flask/app.py
+++ rocketseat-python/curso-100h/3_flask/tasks-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__) 
@@ -8,14 +8,20 @@ app = Flask(__name__)
 # Tabela: Tarefa - criar, ler, atualizar e deletar.
 
 tasks = [] # como não está usando BD, usa uma lista
-
+task_id_control = 1
 
 # POST 
 @app.route("/tasks", methods=['POST'])
 def create_task():
+    global task_id_control # Função terá acesso ao valor original da variável criada fora e poderá atualizar o valor.
+
     data = request.get_json() # request vem do Flask - recuperar as informações
-    print(data)
-    return 'Teste'
+    new_task = Task(id= task_id_control, name=data['title'], description=data.get("description", ""))
+    task_id_control+=1
+
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"}) # Retorno com dicionário que será um json. Melhor para as APIs conseguirem controlar no futuro.
 
 
 
